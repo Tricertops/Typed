c2dca6044ada39fc175ff83a15c1495aaef2c084
Martin Kiss <martin.kiss@me.com>
2014-10-30 10:46:09 +0100

    Initial project setup

---
+++ Test/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  Test
+//
+//  Created by Martin Kiss on 30.10.14.
+//  Copyright (c) 2014 Triceratops. All rights reserved.
+//
+
+#import "Typed.h"
+
+
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        
+        
+    }
+    return EXIT_SUCCESS;
+}
---
+++ Typed.h
@@ -0,0 +1,13 @@
+//
+//  Typed.h
+//  Typed
+//
+//  Created by Martin Kiss on 30.10.14.
+//  Copyright (c) 2014 Triceratops. All rights reserved.
+//
+
+@import Foundation;
+
+
+
+
