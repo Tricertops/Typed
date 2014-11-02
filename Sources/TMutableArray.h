45ddec1f9ef9df584bd5e44aad0e605f6b594329
Martin Kiss <martin.kiss@me.com>
2014-11-02 14:53:36 +0100

    Declare TMutableArray protocol

--- Sources/TMutableArray.h
+++ Sources/TMutableArray.h
@@ -14,5 +14,38 @@
 @class Element; \
 /*! NSMutableArray Interface */ \
 @protocol TMutableArray_##Element <TArray_##Element> \
+/*! Adding Objects */ \
+- (void)addObject:(Element *)object; \
+- (void)addObjectsFromArray:(TArray(Element))otherArray; \
+- (void)insertObject:(Element *)object atIndex:(NSUInteger)index; \
+- (void)insertObjects:(TArray(Element))objects atIndexes:(NSIndexSet *)indexes; \
+/*! Removing Objects */ \
+- (void)removeAllObjects; \
+- (void)removeLastObject; \
+- (void)removeObject:(Element *)object; \
+- (void)removeObject:(Element *)object inRange:(NSRange)range; \
+- (void)removeObjectAtIndex:(NSUInteger)index; \
+- (void)removeObjectsAtIndexes:(NSIndexSet *)indexes; \
+- (void)removeObjectIdenticalTo:(Element *)object; \
+- (void)removeObjectIdenticalTo:(Element *)object inRange:(NSRange)range; \
+- (void)removeObjectsInArray:(TArray(Element))otherArray; \
+- (void)removeObjectsInRange:(NSRange)range; \
+/*! Replacing Objects */ \
+- (void)replaceObjectAtIndex:(NSUInteger)index withObject:(Element *)object; \
+- (void)setObject:(Element *)object atIndexedSubscript:(NSUInteger)index; \
+- (void)replaceObjectsAtIndexes:(NSIndexSet *)indexes withObjects:(TArray(Element))objects; \
+- (void)replaceObjectsInRange:(NSRange)range withObjectsFromArray:(TArray(Element))otherArray range:(NSRange)otherRange; \
+- (void)replaceObjectsInRange:(NSRange)range withObjectsFromArray:(TArray(Element))otherArray; \
+- (void)setArray:(TArray(Element))otherArray; \
+/*! Filtering Array */ \
+- (void)filterUsingPredicate:(NSPredicate *)predicate; \
+/*! Rearranging Content */ \
+- (void)exchangeObjectAtIndex:(NSUInteger)index1 withObjectAtIndex:(NSUInteger)index2; \
+- (void)sortUsingDescriptors:(TArray(NSSortDescriptor))sortDescriptors; \
+- (void)sortUsingComparator:(TArrayComparator_##Element)comparator; \
+- (void)sortWithOptions:(NSSortOptions)options usingComparator:(TArrayComparator_##Element)comparator; \
+- (void)sortUsingFunction:(TArraySortFunction_##Element)compare context:(void *)context; \
+- (void)sortUsingSelector:(SEL)comparator; \
 @end
+
 
